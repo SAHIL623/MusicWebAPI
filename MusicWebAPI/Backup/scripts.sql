36fa8dd2c4545859850cceec97e10f02669c3855
SAHIL623 <79488995+SAHIL623@users.noreply.github.com>
2021-08-17 10:12:07 +1200

    Add files via upload

---
+++ MusicWebAPI/Backup/scripts.sql
@@ -0,0 +1,22 @@
+SET IDENTITY_INSERT [dbo].[Songs] ON 
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (1, N'Call Me By Your Name', N'Lil Nas X', N'https://songwhip.com/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (2, N'Peaches', N'Justin Bieber', N'https://odesli.co/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (3, N'Drivers License', N'Olivia Rodrigo', N'https://odesli.co/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (4, N'Calling My Phone', N'Lil Tjay', N'http://www.noiseaddicts.com/music-links/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (5, N'Up', N'Cardi B', N'http://www.noiseaddicts.com/music-links/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (6, N'Good 4 U', N'Olivia Rodrigo', N'https://freeplaymusic.com/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (7, N'Lost Causes', N'Billie Eilish', N'https://freeplaymusic.com/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (8, N'Bed', N'David Guetta', N'https://www.hungama.com/')
+GO
+INSERT [dbo].[Songs] ([Id], [SongTitle], [Artist], [Url]) VALUES (9, N'What Other People Say', N'Demi Lovato', N'https://www.hungama.com/')
+GO
+SET IDENTITY_INSERT [dbo].[Songs] OFF
+GO
